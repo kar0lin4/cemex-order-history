ce9829e98a607bcbca1f9520430367459404de96
Karolina Kreft <kar.kreft@gmail.com>
2024-07-28 19:11:41 +0200

    feat(cmx-123) add filters service

---
+++ src/app/services/filter.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ActiveFilter } from '../filter/filter.component';
+import { Order } from '../enums/Order';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilterService {
+  private pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
+  private defaultFilter: ActiveFilter = {
+    statuses: [],
+    productLine: '',
+    dateFrom: '',
+    dateTo: '',
+    searchByOrderNumber: '',
+  };
+
+  private filterSubject = new BehaviorSubject<ActiveFilter>(this.defaultFilter);
+  filter$ = this.filterSubject.asObservable();
+
+  updateFilter(newFilter: Partial<ActiveFilter>) {
+    const currentFilter = this.filterSubject.value;
+    const updatedFilter = { ...currentFilter, ...newFilter };
+    this.filterSubject.next(updatedFilter);
+  }
+
+  getFilteredOrders(orders: Order[]): Order[] {
+    const currentFilter = this.filterSubject.value;
+    return orders.filter((order) => {
+      const statusMatch =
+        currentFilter.statuses.length === 0 ||
+        currentFilter.statuses.includes(order.status);
+
+      const lineMatch =
+        currentFilter.productLine === '' ||
+        order.line === currentFilter.productLine;
+
+      const dateFromMatch =
+        !currentFilter.dateFrom ||
+        new Date(
+          order.requestDate.toString().replace(this.pattern, '$3-$2-$1')
+        ) >= new Date(currentFilter.dateFrom);
+
+      const dateToMatch =
+        !currentFilter.dateTo ||
+        new Date(
+          order.requestDate.toString().replace(this.pattern, '$3-$2-$1')
+        ) <= new Date(currentFilter.dateTo);
+
+      const searchByOrderNumberMatch =
+        currentFilter.searchByOrderNumber === '' ||
+        order.orderNumber
+          .toString()
+          .includes(currentFilter.searchByOrderNumber!);
+
+      return (
+        statusMatch &&
+        lineMatch &&
+        dateFromMatch &&
+        dateToMatch &&
+        searchByOrderNumberMatch
+      );
+    });
+  }
+}
