1070ececbbee13656cd84804a52e66c03316cee4
Karolina Kreft <kar.kreft@gmail.com>
2024-07-29 08:52:48 +0200

    Update orders.service.ts

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, retry } from 'rxjs';
 import * as ordersData from '.././../../public/mock_data/orders.json';
 import { Order } from '../enums/Order';
 
@@ -8,7 +8,14 @@ export class OrdersService {
 
   private data = ordersData;
 
-   getOrders(): Observable<Order[]> {
-    return of(this.data.orders);
-  }
+getOrders(): Observable<Order[]> {
+  return of(this.data.orders).pipe(
+    retry(3), 
+    catchError(error => {
+      console.error('Error fetching orders:', error);
+      return of([]); 
+    })
+  );
 }
+}
+
